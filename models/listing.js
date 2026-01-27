44e23eaf70752fca1218b57ee5f9522427b4192e
kushw <kushwahasahitya909@gmail.com>
2026-01-27 20:31:24 +0530

    Added listing model and init.js for database initialisation

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        required:true,
+        default: "https://unsplash.com/photos/blue-body-of-water-in-front-of-building-near-trees-during-nighttime-M7GddPqJowg",
+        set: v=> v==="" ? "https://unsplash.com/photos/blue-body-of-water-in-front-of-building-near-trees-during-nighttime-M7GddPqJowg":v,
+    },
+    price:Number,
+    location:String,
+    country:String
+});
+
+const Listing=mongoose.model('Listing',listingSchema);
+module.exports=Listing;

2a8cabca17579be27a3164d5326f2b12b49bcfa3
kushw <kushwahasahitya909@gmail.com>
2026-03-23 18:21:42 +0530

    Restructured review routes using express router

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const mongourl="mongodb://127.0.0.1:27017/roomify";
-const Listing=require('./models/listing.js');
 const path=require('path');
 const methodOverride=require('method-override');
 const ejsMate=require('ejs-mate');
-const wrapAsync=require('./utils/wrapAsync.js');
 const ExpressError=require('./utils/ExpressError.js');
-const {listingSchema,reviewSchema}=require('./schema.js');
-const Review=require('./models/review.js');
 
 const listings=require('./routes/listing.js');
+const reviews=require('./routes/review.js');
 
-main().then(()=>console.log('Connected to DB'))
+main()
+.then(()=>console.log('Connected to DB'))
 .catch(err=>console.log(err));
+
 async function main() {
     await mongoose.connect(mongourl);
 }
@@ -33,57 +32,11 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/listings',listings); //listing routes
-
-//Review Schema Validation Middleware
-const validateReview=(req,res,next)=>{
-    let {error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map(el=>el.message).join(',');
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
-
-//ADD Review Route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    res.redirect(`/listings/${listing.id}`);
-}))
-
-//Delete Review Route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let {id, reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}}); ///delete review object id from listing array
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}))
-
-//Adding Sample listing
-app.get("/testListing",async(req,res)=>{
-    let sampleListing=new Listing({
-        title:"My New Villa",
-        description:"Near the beach",
-        price:2000,
-        image:"",
-        location:"Panaji,Goa",
-        country:"India"
-    });
-    await sampleListing.save();
-    console.log('sample saved');
-    res.send("successful testing");
-})
+app.use('/listings/:id/reviews',reviews); //review routes
 
 app.use((req,res,next)=>{
     next(new ExpressError(404,'Page not found'));
 })
-
 
 //Error Handler
 app.use((err,req,res,next)=>{

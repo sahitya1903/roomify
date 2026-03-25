c5244fe251c4bf8ebe2170e15c034a5e4d93b226
kushw <kushwahasahitya909@gmail.com>
2026-03-26 01:01:07 +0530

    Added dismissable flash messages for success alerts and failure alerts

--- routes/listing.js
+++ routes/listing.js
@@ -41,6 +41,10 @@ router.post('/',validateListing, wrapAsync(async(req,res,next)=>{
 router.get('/:id',wrapAsync(async(req,res,next)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id).populate('reviews');
+    if(!listing){
+        req.flash('error','listing you requested does not exist');
+        return res.redirect('/listings');
+    }
     res.render('./listings/show.ejs',{listing});
 }))
 
@@ -49,6 +53,10 @@ router.get('/:id',wrapAsync(async(req,res,next)=>{
 router.get('/:id/edit',wrapAsync(async(req,res,next)=>{
     const {id}=req.params;
     const listing=await Listing.findById(id);
+    if(!listing){
+        req.flash('error','listing you requested does not exist');
+        return res.redirect('/listings');
+    }
     res.render('./listings/edit.ejs',{listing});
 }))
 

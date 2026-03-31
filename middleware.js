8a13a37d2776fd85dccda3b020ddcc88bfb22a25
kushw <kushwahasahitya909@gmail.com>
2026-04-01 00:07:17 +0530

    Implementing redirection to saved Url

--- middleware.js
+++ middleware.js
@@ -1,7 +1,19 @@
 module.exports.isLoggedIn=(req,res,next)=>{
+    //save redirect url when user is not logged in
+    // console.log(req.path,'..',req.originalUrl);
     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
         req.flash('error','You must be logged in to create listing.');
         return res.redirect('/login');
+    }
+    next();
+}
+
+
+//needed bcz passport resets redirectUrl value everytime user login successful 
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
 }
--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,13 @@ const wrapAsync = require('../utils/wrapAsync');
 const router=express.Router();
 const User=require('../models/user');
 const passport=require('passport');
+const { saveRedirectUrl } = require('../middleware');
 
 router.get('/signup',(req,res)=>{
     res.render('./users/signup.ejs');
 })
 
-router.post('/signup',wrapAsync(async(req,res)=>{
+router.post('/signup',wrapAsync(async(req,res,next)=>{
     try{
         let {username,email,password}=req.body;
         const newUser=new User({email,username});
@@ -29,11 +30,12 @@ router.get('/login',(req,res)=>{
     res.render('./users/login.ejs');
 })
 
-router.post('/login',
+router.post('/login',saveRedirectUrl,
     passport.authenticate('local',{failureRedirect:'/login',failureFlash:true}),
     wrapAsync(async(req,res)=>{
         req.flash('success','Welcome to Roomify! You are logged in!');
-        res.redirect('/listings');
+        const redirectUrl=res.locals.redirectUrl || '/listings';
+        res.redirect(redirectUrl);
     }))
 
 

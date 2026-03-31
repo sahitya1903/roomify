d7aae253369f6ecb97c20d07c51ffaabc1daf7a7
kushw <kushwahasahitya909@gmail.com>
2026-03-31 22:25:06 +0530

    Implement SignUp and Login user using Middleware in all routes

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const Listing=require('../models/listing.js');
 const wrapAsync=require('../utils/wrapAsync.js');
 const ExpressError=require('../utils/ExpressError.js');
 const {listingSchema}=require('../schema.js');
+const {isLoggedIn}=require('../middleware.js');
 
 
 //Listing Schema Validation Middleware
@@ -18,18 +19,18 @@ const validateListing=(req,res,next)=>{
 }
 
 //INDEX Route
-router.get('/',wrapAsync(async (req,res,next)=>{
+router.get('/',wrapAsync(async (req,res)=>{
     const allListings=await Listing.find({});
     res.render('./listings/index.ejs',{allListings});
 }))
 
 //NEW Route
-router.get('/new',(req,res,next)=>{
-    res.render('./listings/new.ejs')    
+router.get('/new',isLoggedIn,(req,res)=>{
+    res.render('./listings/new.ejs'); 
 })
 
 //CREATE Route
-router.post('/',validateListing, wrapAsync(async(req,res,next)=>{
+router.post('/',isLoggedIn,validateListing, wrapAsync(async(req,res)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     req.flash('success','New Listing Created!');
@@ -38,7 +39,7 @@ router.post('/',validateListing, wrapAsync(async(req,res,next)=>{
 
 
 //SHOW Route
-router.get('/:id',wrapAsync(async(req,res,next)=>{
+router.get('/:id',wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id).populate('reviews');
     if(!listing){
@@ -50,7 +51,7 @@ router.get('/:id',wrapAsync(async(req,res,next)=>{
 
 
 //EDIT Route
-router.get('/:id/edit',wrapAsync(async(req,res,next)=>{
+router.get('/:id/edit',isLoggedIn,wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const listing=await Listing.findById(id);
     if(!listing){
@@ -61,14 +62,14 @@ router.get('/:id/edit',wrapAsync(async(req,res,next)=>{
 }))
 
 //UPDATE Route
-router.put('/:id',validateListing,wrapAsync(async(req,res,next)=>{
+router.put('/:id',isLoggedIn,validateListing,wrapAsync(async(req,res)=>{
     const {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect('/listings');
 }))
 
 //DESTROY Route
-router.delete('/:id',wrapAsync(async(req,res,next)=>{
+router.delete('/:id',isLoggedIn,wrapAsync(async(req,res)=>{
     const {id}=req.params;
     await Listing.findByIdAndDelete(id);
     req.flash('success','Listing Deleted!');

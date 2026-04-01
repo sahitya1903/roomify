ec072b993a100020bd9a68bc2129b32032cb6886
kushw <kushwahasahitya909@gmail.com>
2026-04-02 02:35:27 +0530

    Implemented router.route() function

--- routes/listing.js
+++ routes/listing.js
@@ -4,25 +4,20 @@ const wrapAsync=require('../utils/wrapAsync.js');
 const {isLoggedIn, isOwner, validateListing}=require('../middleware.js');
 const listingController=require('../controllers/listing.js');
 
-//INDEX Route
-router.get('/',wrapAsync(listingController.index));
+router.route('/')
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn,validateListing,
+    wrapAsync(listingController.createListing));
 
 //NEW Route
 router.get('/new',isLoggedIn,listingController.renderNewForm);
 
-//CREATE Route
-router.post('/',isLoggedIn,validateListing, wrapAsync(listingController.createListing))
+router.route('/:id')
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
 
-//SHOW Route
-router.get('/:id',wrapAsync(listingController.showListing))
-
-//EDIT Route
+//EDIT Route 
 router.get('/:id/edit',isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm))
-
-//UPDATE Route
-router.put('/:id',isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
-
-//DESTROY Route
-router.delete('/:id',isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,13 @@ const passport=require('passport');
 const { saveRedirectUrl } = require('../middleware');
 const userController=require('../controllers/user');
 
-router.get('/signup',userController.renderSignupForm);
+router.route('/signup')
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup));
 
-router.post('/signup',wrapAsync(userController.signup));
-
-router.get('/login',userController.renderLoginForm);
-
-router.post('/login',saveRedirectUrl,
+router.route('/login')
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,
     passport.authenticate('local',{failureRedirect:'/login',failureFlash:true}),
     wrapAsync(userController.login)
 );

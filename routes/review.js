7021a12495e7fc23678485e346e7cc9c385c137a
kushw <kushwahasahitya909@gmail.com>
2026-04-02 02:10:09 +0530

    Implemented MVC for Users and Reviews

--- routes/review.js
+++ routes/review.js
@@ -1,31 +1,13 @@
 const express=require('express');
 const router=express.Router({mergeParams:true});
-const Listing=require('../models/listing.js');
-const Review=require('../models/review.js');
 const wrapAsync=require('../utils/wrapAsync.js');
 const {validateReview, isLoggedIn, isReviewAuthor}=require('../middleware.js');
+const reviewController=require('../controllers/review.js');
 
 //ADD Review Route
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-    newReview.author=req.user._id;
-    console.log(newReview);
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash('success','New Review Created!');
-    res.redirect(`/listings/${listing.id}`);
-}))
+router.post("/",validateReview,wrapAsync(reviewController.createReview));
 
 //Delete Review Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
-    let {id, reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}}); ///delete review object id from listing array
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Review Deleted!');
-    res.redirect(`/listings/${id}`);
-}))
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.deleteReview));
 
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,21 @@
 const express=require('express');
 const wrapAsync = require('../utils/wrapAsync');
 const router=express.Router();
-const User=require('../models/user');
 const passport=require('passport');
 const { saveRedirectUrl } = require('../middleware');
+const userController=require('../controllers/user');
 
-router.get('/signup',(req,res)=>{
-    res.render('./users/signup.ejs');
-})
+router.get('/signup',userController.renderSignupForm);
 
-router.post('/signup',wrapAsync(async(req,res,next)=>{
-    try{
-        let {username,email,password}=req.body;
-        const newUser=new User({email,username});
-        const registeredUser=await User.register(newUser,password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err)=>{       //automatic login after signup
-            if(err) return next(err);
-            req.flash('success','Welcome to Roomify');
-            res.redirect('/listings');
-        })
-    }catch(err){
-        req.flash('error',err.message);
-        res.redirect('/signup')
-    }  
-}))
+router.post('/signup',wrapAsync(userController.signup));
 
-router.get('/login',(req,res)=>{
-    res.render('./users/login.ejs');
-})
+router.get('/login',userController.renderLoginForm);
 
 router.post('/login',saveRedirectUrl,
     passport.authenticate('local',{failureRedirect:'/login',failureFlash:true}),
-    wrapAsync(async(req,res)=>{
-        req.flash('success','Welcome to Roomify! You are logged in!');
-        const redirectUrl=res.locals.redirectUrl || '/listings';
-        res.redirect(redirectUrl);
-    }))
+    wrapAsync(userController.login)
+);
 
-
-router.get('/logout',(req,res,next)=>{
-    req.logOut(err=>{
-        if(err){
-            return next();
-        }else{
-            req.flash('success','You are logged out!');
-            res.redirect('/listings');
-        }
-    });
-})
+router.get('/logout',userController.logout);
 
 module.exports=router;

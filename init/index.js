0656da2cf9b4b57feb1b26a1610616fe58ff2257
kushw <kushwahasahitya909@gmail.com>
2026-04-01 23:00:31 +0530

    Adding listing owner attribute

--- init/index.js
+++ init/index.js
@@ -12,6 +12,7 @@ async function main() {
 
 const initDB=async ()=>{
     await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({...obj,owner: '69cb701ec9dc1a3d444033da'}))
     await Listing.insertMany(initData.data);
     console.log('data was initialised');
 }
--- routes/listing.js
+++ routes/listing.js
@@ -32,6 +32,7 @@ router.get('/new',isLoggedIn,(req,res)=>{
 //CREATE Route
 router.post('/',isLoggedIn,validateListing, wrapAsync(async(req,res)=>{
     const newListing=new Listing(req.body.listing);
+    newListing.owner=req.user._id;
     await newListing.save();
     req.flash('success','New Listing Created!');
     res.redirect('/listings');
@@ -41,11 +42,12 @@ router.post('/',isLoggedIn,validateListing, wrapAsync(async(req,res)=>{
 //SHOW Route
 router.get('/:id',wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    const listing=await Listing.findById(id).populate('reviews');
+    const listing=await Listing.findById(id).populate('reviews').populate('owner');
     if(!listing){
         req.flash('error','listing you requested does not exist');
         return res.redirect('/listings');
     }
+    console.log(listing);
     res.render('./listings/show.ejs',{listing});
 }))
 

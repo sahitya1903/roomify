95b0d033c0c53c390398c32175c68da209eaa307
kushw <kushwahasahitya909@gmail.com>
2026-04-05 18:16:15 +0530

    Tested local image upload functionality

--- routes/listing.js
+++ routes/listing.js
@@ -3,11 +3,17 @@ const router=express.Router();
 const wrapAsync=require('../utils/wrapAsync.js');
 const {isLoggedIn, isOwner, validateListing}=require('../middleware.js');
 const listingController=require('../controllers/listing.js');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 
 router.route('/')
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn,validateListing,
-    wrapAsync(listingController.createListing));
+    // .post(isLoggedIn,validateListing,
+    // wrapAsync(listingController.createListing));
+    .post(upload.single('listing[image]'),(req,res)=>{
+        res.send(req.file);
+    })
 
 //NEW Route
 router.get('/new',isLoggedIn,listingController.renderNewForm);

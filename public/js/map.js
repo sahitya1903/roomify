3958d5cb18826f0551bca15c6235dd1d0c0dfa57
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-06 16:57:37 +0530

    Added map popup feature

--- public/js/map.js
+++ public/js/map.js
@@ -4,11 +4,15 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
     zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-    center: coordinates // center the map on this longitude and latitude
+    center: listing.geometry.coordinates // center the map on this longitude and latitude
 });
 
-console.log(coordinates);
 
 const marker = new mapboxgl.Marker({color: 'red'})
-    .setLngLat(coordinates)
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(
+        `<h6>${listing.title}</h5>
+        <p>Welcome to your Hotel Room! Exact location will be provided after booking.</p>`
+    ))
     .addTo(map);

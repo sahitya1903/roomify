77eefb4d01678aecee20cbd4bd6acbd523b24913
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-06 16:38:05 +0530

    Implemented map marker

--- public/js/map.js
+++ public/js/map.js
@@ -4,12 +4,11 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
     zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-    center: [77.216721, 28.6448] // center the map on this longitude and latitude
+    center: coordinates // center the map on this longitude and latitude
 });
 
-map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
+console.log(coordinates);
 
-map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
-});
+const marker = new mapboxgl.Marker({color: 'red'})
+    .setLngLat(coordinates)
+    .addTo(map);

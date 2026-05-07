777d469164b01634e8de12f87a735f70cc03a942
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-07 16:18:04 +0530

    First deploy

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret: 'mysecret'
+        secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
 })
@@ -60,7 +60,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
     store,
-    secret: 'mysupersecret',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
